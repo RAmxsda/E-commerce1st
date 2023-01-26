4a7484208bf27fceaef2441f2c5bfd0e65e034d2
Ramesh <101050352+RAmxsda@users.noreply.github.com>
2023-01-26 19:38:27 +0545

    dashboard extending functionalites

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Account
+from .models import *
+from django.utils.html import format_html
 
 # Register your models here.
 
@@ -16,3 +17,11 @@ class AccountAdmin(UserAdmin):
 
     
 admin.site.register(Account,AccountAdmin)
+
+class UserProfileAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="70" height="50" style="border-radius:50%;">'.format(object.profile_picture.url))
+    thumbnail.short_description = 'Profile Picture'
+    list_display = ('thumbnail', 'user', 'city', 'state', 'country')
+
+admin.site.register(UserProfile,UserProfileAdmin)
